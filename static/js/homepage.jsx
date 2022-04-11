ac2fea9225276db8ec324dbef3559c0b9417ed2d
Michelle Song <michssong@gmail.com>
2022-04-10 23:12:44 -0700

    Added homepage.jsx info

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,13 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return (
+  <div>
+    <p>Welcome to a boring site for cards...</p>
+    <a href="/cards">Click me to see cards</a>
+    <img src="/static/img/balloonicorn.jpg" />
+  </div>
+  );
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
